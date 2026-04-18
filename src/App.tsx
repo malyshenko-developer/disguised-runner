c05222734ac41814276c9b48ab83342c00e411da
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-04-18 13:31:34 +0800

    init: initial project, add dependencies

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className={"flex items-center justify-center h-screen"}>
+      <p className={"text-cyan-300 text-6xl"}>DINO Runner</p>
+    </div>
+  )
+}
+
+export default App
