c3f5987cec29e93d45390ef96a3a580c811ad40c
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-04-19 13:16:10 +0800

    feat: parallax movement with useTick + deltaTime

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,42 +1,13 @@
-import {useEffect, useState} from "react";
 import {Application} from "@pixi/react";
-import {Assets, TextStyle, Texture} from "pixi.js";
+import {TextStyle} from "pixi.js";
+
 import {Hero} from "./Hero.tsx";
+import {ParallaxBg} from "./ParallaxBg.tsx";
 
 export const GameCanvas = () => {
-    const [textures, setTextures] = useState<{ [key: string]: Texture }>({});
-
-    const layerOrder = [
-        'Layer_0011_0', 'Layer_0010_1', 'Layer_0009_2', 'Layer_0008_3',
-        'Layer_0007_Lights', 'Layer_0006_4', 'Layer_0005_5', 'Layer_0004_Lights',
-        'Layer_0003_6', 'Layer_0002_7', 'Layer_0001_8', 'Layer_0000_9'
-    ];
-
-    useEffect(() => {
-        layerOrder.forEach(name => {
-            Assets.load(`/sprites/background/${name}.png`).then(texture => {
-                setTextures(prev => ({ ...prev, [name]: texture }));
-            });
-        });
-    }, []);
-
     return (
         <Application width={1200} height={600} className="border-2 border-[#596E84] rounded">
-            {layerOrder.map((name) => {
-                const texture = textures[name];
-                if (!texture) return null;
-
-                return (
-                    <pixiTilingSprite
-                        key={name}
-                        texture={texture}
-                        tilePosition={{ x: 0, y: 620 }}
-                        width={1200}
-                        height={600}
-                        tileScale={{ x: 1, y: 1 }}
-                    />
-                );
-            })}
+            <ParallaxBg />
 
             <pixiText
                 text="DISGUISED RUNNER"
---
+++ src/components/ParallaxBg.tsx
@@ -0,0 +1,49 @@
+import {useEffect, useState} from "react";
+import {useTick} from "@pixi/react";
+import {Assets, Texture} from "pixi.js";
+
+export const ParallaxBg = () => {
+    const [textures, setTextures] = useState<{ [key: string]: Texture }>({});
+    const [scrollX, setScrollX] = useState(0);
+
+    const layerOrder = [
+        'Layer_0011_0', 'Layer_0010_1', 'Layer_0009_2', 'Layer_0008_3',
+        'Layer_0007_Lights', 'Layer_0006_4', 'Layer_0005_5', 'Layer_0004_Lights',
+        'Layer_0003_6', 'Layer_0002_7', 'Layer_0001_8', 'Layer_0000_9'
+    ];
+
+    useEffect(() => {
+        layerOrder.forEach(name => {
+            Assets.load(`/sprites/background/${name}.png`).then(texture => {
+                setTextures(prev => ({ ...prev, [name]: texture }));
+            });
+        });
+    }, []);
+
+    useTick((ticker) => {
+        setScrollX(s => s +  2 * ticker.deltaTime)
+    })
+
+
+    return (
+        <>
+            {layerOrder.map((name, index) => {
+                const texture = textures[name];
+                if (!texture) return null;
+
+                const speed = index * 0.08
+
+                return (
+                    <pixiTilingSprite
+                        key={name}
+                        texture={texture}
+                        tilePosition={{ x: -scrollX * speed, y: 620 }}
+                        width={1200}
+                        height={600}
+                        tileScale={{ x: 1, y: 1 }}
+                    />
+                );
+            })}
+        </>
+    )
+}
