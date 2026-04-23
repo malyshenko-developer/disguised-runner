19597b34b2795fc54b3e9596025df4d92b37086f
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-04-23 19:17:59 +0800

    feat: mushroom enemy component

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,15 +1,36 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Application} from "@pixi/react";
 
 import {Hero} from "./Hero.tsx";
 import {ParallaxBg} from "./ParallaxBg.tsx";
 import {PlayButton} from "./PlayButton.tsx";
+import {Enemy} from "./Enemy.tsx";
+
+interface EnemyData {
+    id: number;
+    initialX: number;
+}
 
 export const GameCanvas = () => {
     const [scrollX, setScrollX] = useState<number>(0);
     const [gameRunning, setGameRunning] = useState<boolean>(false);
+    const [enemies, setEnemies] = useState<EnemyData[]>([]);
+
+    const spawnEnemy = () => {
+        const id = Date.now();
+        setEnemies(prev => [...prev, {id, initialX: 1400}]);
+    };
 
     const toggleGame = () => setGameRunning(true);
+
+    useEffect(() => {
+        if (!gameRunning) return;
+
+        spawnEnemy();
+
+        const interval = setInterval(spawnEnemy, 3000);
+        return () => clearInterval(interval);
+    }, [gameRunning]);
 
     return (
         <Application width={1200} height={600} className="border-2 border-[#596E84] rounded">
@@ -19,20 +40,14 @@ export const GameCanvas = () => {
                 <PlayButton running={gameRunning} onToggle={toggleGame} />
             )}
 
-            {/*<pixiText*/}
-            {/*    text="DISGUISED RUNNER"*/}
-            {/*    anchor={{ x: 0.5, y: 0.5 }}*/}
-            {/*    x={600}*/}
-            {/*    y={280}*/}
-            {/*    style={new TextStyle({*/}
-            {/*        fontFamily: 'Arial Black, Impact',*/}
-            {/*        fontSize: 48,*/}
-            {/*        fill: 0xE8F4FD,*/}
-            {/*        stroke: 0x1A2336,*/}
-            {/*        dropShadow: true,*/}
-            {/*    })}*/}
-            {/*/>*/}
-
             <Hero scrollX={scrollX} running={gameRunning} />
+
+            {enemies.map(({id, initialX}) => (
+                <Enemy
+                    key={id}
+                    initialX={initialX}
+                    onDestroy={() => setEnemies(e => e.filter(enemy => enemy.id !== id))}
+                />
+            ))}
         </Application>
     )};
